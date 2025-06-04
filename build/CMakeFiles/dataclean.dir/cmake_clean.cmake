15b55d73a6b37886674ef8f4cf5bac668a375633
romanlee <romanlee@lbl.gov>
2025-06-03 17:30:23 -0700

    Untrack build/

--- build/CMakeFiles/dataclean.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dataclean"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dataclean.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/forallTest.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for forallTest.
-# This may be replaced when dependencies are built.
