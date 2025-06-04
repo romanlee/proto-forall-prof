7163042e4266f64824ff6ab3b554d5187e02fb4f
romanlee <romanlee@lbl.gov>
2025-06-03 17:41:59 -0700

    Clean me up a little bit more

--- roman/build-n-test-stuffs.sh
+++
@@ -1,6 +0,0 @@
-rm -r build; rm roman/*/TIMINGS.txt
-# cmake -B build -D DIM=2 -D ENABLE_MPI=ON -D AMR=ON -D MMB=ON -D ENABLE_CUDA=ON -D ENABLE_HIP=OFF -D ENABLE_HDF5=ON -D OPS=ON -D DEBUG=ON -D VERBOSE=0 -D ENABLE_ALL_TESTS=ON -D DISABLE_INIT_CHECKS=0 -D ENABLE_ROMAN=ON
-cmake -B build -D DIM=2 -D ENABLE_CUDA=ON -D DEBUG=ON -D VERBOSE=0 -D ENABLE_ROMAN=ON
-cmake --build build/ --target LightKernel -j 22
-
-nsys profile -o roman/LightKernel/LightKernel%n ./build/bin/LightKernel
--- stuff.sh
+++ stuff.sh
@@ -1,9 +1,8 @@
 # configure
-rm -r build/
-cmake -B build -D DIM=2 -D ENABLE_CUDA=ON -D DEBUG=ON -D VERBOSE=0
+rm -r build/; cmake -B build -D ENABLE_CUDA=ON -D DEBUG=ON -D ENABLE_MPI=OFF -D VERBOSE=0 -D DIM=2
 
 # build
-cmake --build build/ --target LightKernel -j 22
+cmake --build build/ --target forallTest -j 22
 
 # profile
 nsys profile -o roman/LightKernel/LightKernel%n ./build/bin/LightKernel
